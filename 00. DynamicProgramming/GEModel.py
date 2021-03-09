cd26d902bd66d6706f1c7fd86d1bb7cecc4bdfa7
Jeppe Druedahl <jeppe.druedahl@econ.ku.dk>
2021-03-09 15:38:38 +0100

    Fast computation of Jacobians

--- 00. DynamicProgramming/GEModel.py
+++ 00. DynamicProgramming/GEModel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """GEModelClass
 
-Solves and simulates a buffer-stock consumption-saving problem for use in a general equilibrium model
+Solves an Aiygari model
 
 """
 
@@ -101,6 +101,8 @@ class GEModelClass(ModelClass):
         sol.m = np.zeros(sol_shape)
         sol.c = np.zeros(sol_shape)
         sol.Va = np.zeros(sol_shape)
+        sol.i = np.zeros(sol_shape,dtype=np.int_)
+        sol.w = np.zeros(sol_shape)
 
         # path
         path_sol_shape = (par.path_T,par.Ne,par.Na)
@@ -108,23 +110,25 @@ class GEModelClass(ModelClass):
         sol.path_m = np.zeros(path_sol_shape)
         sol.path_c = np.zeros(path_sol_shape)
         sol.path_Va = np.zeros(path_sol_shape)
+        sol.path_i = np.zeros(path_sol_shape,dtype=np.int_)
+        sol.path_w = np.zeros(path_sol_shape)
 
         # c. simulation
         sim_shape = sol_shape
-        sim.i = np.zeros(sim_shape,dtype=np.int_)
-        sim.w = np.zeros(sim_shape)
         sim.D = np.zeros(sim_shape)
 
         # path
         path_sim_shape = path_sol_shape
-        sim.path_i = np.zeros(path_sim_shape,dtype=np.int_)
-        sim.path_w = np.zeros(path_sim_shape)
         sim.path_D = np.zeros(path_sim_shape)
         sim.path_K = np.zeros(par.path_T)
         sim.path_C = np.zeros(par.path_T)
-
+        sim.path_Klag = np.zeros(par.path_T)
+        
         # jacobians
         jac_shape = (par.path_T,par.path_T)
+
+        sol.jac_K = np.zeros(jac_shape)
+        sol.jac_C = np.zeros(jac_shape)
 
         sol.jac_curlyK_r = np.zeros(jac_shape)
         sol.jac_curlyK_w = np.zeros(jac_shape)
@@ -232,55 +236,39 @@ class GEModelClass(ModelClass):
             print(f'goods market clearing: {par.Y_ss-par.C_ss-par.delta*par.K_ss:12.8f}')
 
     def solve_household_ss(self,r,Va=None,do_print=False):
-        """ gateway for solving the model in steady state """
+        """ solve the household problem in steady state """
         
         t0 = time.time()
-
+        
         with jit(self) as model:
             
-            success = True
-            try:
+            par = model.par
+            sol = model.sol
+            
+            # a. find wage from optimal firm behavior
+            w = self.implied_w(r,par.Z)
 
-                par = model.par
-                sol = model.sol
-                
-                # a. find wage from optimal firm behavior
-                w = self.implied_w(r,par.Z)
+            # b. initial guess
+            sol.m[:,:] = (1+r)*par.a_grid[np.newaxis,:] + w*par.e_grid[:,np.newaxis]
+            sol.Va[:,:] = (1+r)*(0.1*sol.m)**(-par.sigma) if Va is None else Va
 
-                # b. create (or re-create) grids
-                self.create_grids()
+            # c. solve
+            it = solve_ss(par,sol,r,w)
 
-                # c. solve
-                sol.m[:,:] = (1+r)*par.a_grid[np.newaxis,:] + w*par.e_grid[:,np.newaxis]
-                sol.Va[:,:] = (1+r)*(0.1*sol.m)**(-par.sigma) if Va is None else Va
+            # d. indices and weights    
+            find_i_and_w(par,sol.a,sol.i,sol.w)
 
-                it = 0
-                while True:
-
-                    # i. save
-                    a_old = sol.a.copy()
-
-                    # ii. egm
-                    time_iteration(par,r,w,sol.Va,sol.Va,sol.a,sol.c,sol.m)
-
-                    # ii. check
-                    if np.max(np.abs(sol.a-a_old)) < par.solve_tol: break
-                    
-                    # iv. increment
-                    it += 1
-                    if it > par.max_iter_solve: raise Exception('too many iterations when solving for steady state')
-
-            except:
-
-                success = False
-                
         if do_print:
-            print(f'household problem solved in {elapsed(t0)} [{it} iterations]')
 
-        return success
+            if it >= 0:
+                print(f'household problem solved in {elapsed(t0)} [{it} iterations]')
+            else:
+                print(f'household problem solution did not converge')
+
+        return (it >= 0)
 
     def solve_household_path(self,path_r,path_w,do_print=False):
-        """ gateway for solving the model along price path (with optimal update of path for w) """
+        """ solve household problem along the transition path """
 
         t0 = time.time()
 
@@ -289,31 +277,11 @@ class GEModelClass(ModelClass):
             par = model.par
             sol = model.sol
 
-            # a. create (or re-create) grids
-            self.create_grids()
-
-            # c. solve
-            for t in reversed(range(par.path_T)):
-                
-                # i. prices
-                r = path_r[t]
-                w = path_w[t]
-
-                # ii. next-period
-                if t == par.path_T-1:
-                    Va_p = sol.Va
-                else:
-                    Va_p = sol.path_Va[t+1]
-
-                # ii. solve
-                sol.path_m[t] = (1+r)*par.a_grid[np.newaxis,:] + w*par.e_grid[:,np.newaxis]
-
-                # iii. time iteration
-                time_iteration(par,r,w,Va_p,sol.path_Va[t],sol.path_a[t],sol.path_c[t],sol.path_m[t])
+            solve_path(par,sol,path_r,path_w)
 
         if do_print:
             print(f'household problem solved in {elapsed(t0)}')
-
+        
     def simulate_household_ss(self,D=None,do_print=False):
         """ gateway for simulating the model towards the steady state"""
         
@@ -321,27 +289,24 @@ class GEModelClass(ModelClass):
 
         with jit(self) as model:
 
-            success = True
-            try:
+            par = model.par
+            sol = model.sol
+            sim = model.sim        
 
-                par = model.par
-                sol = model.sol
-                sim = model.sim        
+            # a. intial guess
+            sim.D[:,:] = 0.0
+            sim.D[:,0] = par.e_ergodic # start with a = 0.0 for everybody
+            
+            # b. simulate
+            it = simulate_ss(par,sol,sim)
 
-                # a. intial guess
-                D = (np.repeat(par.e_ergodic,par.Na)/par.Na).reshape(par.Ne,par.Na) if D is None else D
-
-                # b. simulate
-                it = simulate_ss(par,sol,sim,D)
-
-                if do_print:
+            if do_print:
+                if it >= 0:
                     print(f'household problem simulated in {elapsed(t0)} [{it} iterations]')
+                else:
+                    print(f'household problem simulation did not converge')
 
-            except:
-
-                success = False
-
-        return success
+        return (it >= 0)
         
     def simulate_household_path(self,D0,do_print=False):
         """ gateway for simulating the model along path"""
@@ -359,13 +324,29 @@ class GEModelClass(ModelClass):
         if do_print:
             print(f'household problem simulated in {elapsed(t0)}')
 
+    def simulate_household_path_jac(self,D0,dprice,do_print=False):
+        """ gateway for simulating the model along path"""
+        
+        t0 = time.time()
+
+        with jit(self) as model:
+
+            par = model.par
+            sol = model.sol
+            sim = model.sim        
+
+            simulate_path_jac(par,sol,sim,D0,dprice)
+
+        if do_print:
+            print(f'household problem simulated in {elapsed(t0)}')
+
 ######################
 # fast jit functions #
 ######################
 
 @njit(parallel=True)        
-def time_iteration(par,r,w,Va_p,Va,a,c,m):
-    """ perform time iteration step with Va_p from previous iteration """
+def solve_backwards(par,r,w,Va_p,Va,a,c,m):
+    """ solve backwards with Va_p from previous iteration """
 
     # a. post-decision 
     marg_u_plus = (par.beta*par.e_trans)@Va_p
@@ -379,41 +360,91 @@ def time_iteration(par,r,w,Va_p,Va,a,c,m):
 
         # ii. interpolation
         linear_interp.interp_1d_vec(m_endo,par.a_grid,m[i_e],a[i_e])
-        a[i_e,0] = np.fmax(a[i_e,0],0)
+        a[i_e,0] = np.fmax(a[i_e,0],0) # enforce borrowing constraint
         c[i_e] = m[i_e]-a[i_e]
 
         # iii. envelope condition
         Va[i_e] = (1+r)*c[i_e]**(-par.sigma)
     
 @njit
+def solve_ss(par,sol,r,w):
+    """ solve household problem in steady state """
+
+    it = 0
+    while True:
+
+        # i. save
+        a_old = sol.a.copy()
+
+        # ii. egm
+        solve_backwards(par,r,w,sol.Va,sol.Va,sol.a,sol.c,sol.m)
+
+        # ii. check
+        if np.max(np.abs(sol.a-a_old)) < par.solve_tol: 
+            return it
+        
+        # iv. increment
+        it += 1
+        if it > par.max_iter_solve: 
+            return -1
+        
+@njit
+def solve_path(par,sol,path_r,path_w):
+    """ solve household problem along the transition path """
+
+    for k in range(par.path_T):
+
+        t = (par.path_T-1)-k
+
+        # a. prices
+        r = path_r[t]
+        w = path_w[t]
+
+        # b. next-period
+        if t == par.path_T-1:
+            Va_p = sol.Va
+        else:
+            Va_p = sol.path_Va[t+1]
+
+        # c. solve
+        for i_e in range(par.Ne):
+            for i_a in range(par.Na):
+                sol.path_m[t,i_e,i_a] = (1+r)*par.a_grid[i_a] + w*par.e_grid[i_e]
+
+        # d. time iteration
+        solve_backwards(par,r,w,Va_p,sol.path_Va[t],sol.path_a[t],sol.path_c[t],sol.path_m[t])
+
+        # e. find indices and weights
+        find_i_and_w(par,sol.path_a[t],sol.path_i[t],sol.path_w[t])
+
+    return k
+        
+@njit
 def binary_search(imin,x,xi):
     """ binary search algorithm """
 
-    Nx = x.size
+    Na = x.size
 
     # a. checks
     if xi <= x[0]:
         return 0
-    elif xi >= x[Nx-2]:
-        return Nx-2
+    elif xi >= x[Na-2]:
+        return Na-2
     
     # b. binary search
-    half = Nx//2
+    half = Na//2
     while half:
         imid = imin + half
         if x[imid] <= xi:
             imin = imid
-        Nx -= half
-        half = Nx//2
+        Na -= half
+        half = Na//2
         
     return imin
 
 @njit(parallel=True) 
-def find_i_and_w(par,sim,a):
+def find_i_and_w(par,a,i,w):
     """ find indices and weights for simulation """
-
-    i = sim.i
-    w = sim.w
 
     for i_e in prange(par.Ne):
         for i_a in prange(par.Na):
@@ -424,73 +455,144 @@ def find_i_and_w(par,sim,a):
             # b. find i_ such par.a_grid[i_] <= a_ < par.a_grid[i_+1]
             i_ = i[i_e,i_a] = binary_search(0,par.a_grid,a_) 
 
-            # b. weight
+            # c. weight
             w[i_e,i_a] = (par.a_grid[i_+1] - a_) / (par.a_grid[i_+1] - par.a_grid[i_])
 
+            # d. bound simulation at upper grid point
+            w[i_e,i_a] = np.fmin(w[i_e,i_a],1.0)
+
 @njit(parallel=True)   
-def simulate(par,sim,e_trans_T,D):
-    """ simulate given weight indices are weight """
+def simulate_forwards(D_lag,i,w,e_trans_T,D):
+    """ simulate given indices and weights """
 
-    # a. assuming e is constant
-    D_upd = np.zeros(D.shape)
-    for i_e in prange(par.Ne):
-        for i_a in range(par.Na):
+    Ne,Na = D_lag.shape
+
+    # a. assuming e is constant 
+    # (same as multiplication with Q transposed)
+    for i_e in prange(Ne):
+        
+        D[i_e,:] = 0
+        for i_a in range(Na):
             
-            # a. from
-            D_ = D[i_e,i_a]
-            w = sim.w[i_e,i_a]
+            # i. from
+            D_ = D_lag[i_e,i_a]
 
-            # b. to
-            i = sim.i[i_e,i_a]            
-            D_upd[i_e,i] += D_*w
-            D_upd[i_e,i+1] += D_*(1.0-w)
+            # i. to
+            i_ = i[i_e,i_a]            
+            w_ = w[i_e,i_a]
+            D[i_e,i_] += D_*w_
+            D[i_e,i_+1] += D_*(1.0-w_)
     
     # b. account for transition of e
-    D_upd_ = e_trans_T@D_upd
+    # (same as multiplication with tilde Pi transposed)
+    D[:,:] = e_trans_T@D
 
-    return D_upd_
+@njit(parallel=True)   
+def simulate_forwards_transpose(D_lag,i,w,e_trans,D):
+    """ simulate given indices and weights """
 
-@njit(parallel=True)        
-def simulate_ss(par,sol,sim,D):
+    # a. account for transition e
+    # (same as multiplication with tilde Pi)
+    D_temp = e_trans@D_lag
+    
+    # b. assuming e is constant
+    # (same as multiplication with Q)    
+    Ne, Na = D.shape
+    for i_e in prange(Ne):
+        for i_a in prange(Na):
+            i_ = i[i_e,i_a]
+            w_ = w[i_e,i_a]
+            D[i_e,i_a] = w_*D_temp[i_e,i_] + (1.0-w_)*D_temp[i_e,i_+1]
+
+@njit
+def simulate_ss(par,sol,sim):
     """ simulate forward to steady state """
 
-    # a. indices and weights
-    find_i_and_w(par,sim,sol.a)
+    # a. prepare
+    e_trans_T = par.e_trans.T.copy()
+    D_lag = np.zeros(sim.D.shape)
 
     # b. iterate
-    e_trans_T = par.e_trans.T.copy()
     it = 0
     while True:
-
+        
         # i. update distribution
-        D_upd = simulate(par,sim,e_trans_T,D)
+        D_lag[:,:] = sim.D
+        simulate_forwards(D_lag,sol.i,sol.w,e_trans_T,sim.D)
 
-        # ii. check
-        if np.max(np.abs(D_upd-D)) < par.simulate_tol: break
-        D = D_upd
+        # ii. check convergence
+        if np.max(np.abs(sim.D-D_lag)) < par.simulate_tol: 
+            return it
 
         # iii. increment
         it += 1
-        if it > par.max_iter_simulate: raise Exception()
-
-    # d. save
-    sim.D[:,:] = D
-    return it
+        if it > par.max_iter_simulate: 
+            return -1
     
-@njit    
+@njit
 def simulate_path(par,sol,sim,D0):
     """ simulate along path """
 
     e_trans_T = par.e_trans.T.copy()
     for t in range(par.path_T):
 
-        # a. update distribution
+        # a. lag
         if t == 0:
-            sim.path_D[t] = D0 # initial distribution is fixed
+            D_lag = D0
         else:
-            find_i_and_w(par,sim,sol.path_a[t-1])
-            sim.path_D[t] = simulate(par,sim,e_trans_T,sim.path_D[t-1])
+            D_lag = sim.path_D[t-1]
 
-        # b. capital
-        sim.path_K[t] = np.sum(sol.path_a[t]*sim.path_D[t])
-        sim.path_C[t] = np.sum(sol.path_c[t]*sim.path_D[t])
+        # b. unpack
+        path_a = sol.path_a[t]
+        path_c = sol.path_c[t]
+        path_i = sol.path_i[t]
+        path_w = sol.path_w[t]
+
+        # c. aggregate
+        sim.path_K[t] = np.sum(path_a*D_lag)
+        sim.path_C[t] = np.sum(path_c*D_lag)
+
+        # d. update distribution
+        simulate_forwards(D_lag,path_i,path_w,e_trans_T,sim.path_D[t])
+
+    # e. lagged capital
+    sim.path_Klag[0] = np.sum(par.a_grid*D0)
+    sim.path_Klag[1:] = sim.path_K[:-1]
+    
+@njit
+def simulate_path_jac(par,sol,sim,D0,dprice):
+    """ simulate along path """
+
+    e_trans_T = par.e_trans.T.copy()
+
+    # a. simulate when initial policy is not steady state
+    for s in range(par.path_T):
+        for t in range(par.path_T):
+
+            time_to_shock = s-t
+            
+            # i. lag
+            if t == 0:
+                D_lag = D0
+            else:
+                D_lag = sim.path_D[t-1]
+
+            # ii. unpack
+            if time_to_shock >= 0: # only time to shock matter
+                t_ = (par.path_T-1)-time_to_shock
+                path_a = sol.path_a[t_]
+                path_c = sol.path_c[t_]
+                path_i = sol.path_i[t_]
+                path_w = sol.path_w[t_]
+            else: # steady state solution
+                path_a = sol.a
+                path_c = sol.c
+                path_i = sol.i
+                path_w = sol.w
+
+            # iii. aggregate
+            sol.jac_K[t,s] = (np.sum(path_a*D_lag)-par.K_ss)/dprice
+            sol.jac_C[t,s] = (np.sum(path_c*D_lag)-par.C_ss)/dprice
+                
+            # iv. simulate forward    
+            simulate_forwards(D_lag,path_i,path_w,e_trans_T,sim.path_D[t])
